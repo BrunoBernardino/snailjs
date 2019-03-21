7e398e0d8e8ea6b3cbd9409e55bb5ec83f692f74
Bruno Bernardino <bruno@bernardino.io>
2019-03-21 15:26:03 +0000

    Allow flow type checking on the backend.

--- webpack.config.backend.js
+++ webpack.config.backend.js
@@ -17,6 +17,17 @@ module.exports = {
   node: {
     __dirname: false,
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
   externals: [nodeExternals()],
   resolve: {
     modules: [path.resolve(__dirname, 'backend'), 'node_modules'],
